7acba60b84694fce33c1014248c86febdbbd6b1d
mkuba50 <mkuba50@outlook.com>
2017-02-11 23:43:38 +0100

    Add "Microsoft JhengHei" font to support Japanese language

--- web/shared/style.php
+++ web/shared/style.php
@@ -68,7 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <style>
-        body{font-family: "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif; padding-top: 50px;}
+        body{font-family: "Segoe UI", "Microsoft JhengHei", "Helvetica Neue", Helvetica, Arial, sans-serif; padding-top: 50px;}
         .content {padding: 30px 15px;}
         .prod-btn {margin-top: 1em;}
         .prod-btn-title {text-align: left; white-space: normal;}
